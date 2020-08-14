b2d107981504086e7813b5987c8e9a785265b2cd
antony-jr <antonyjr@protonmail.com>
2020-08-15 03:02:45 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="shell_control", # Replace with your own username
+    version="0.0.1",
+    author="Antony Jr.",
+    author_email="antonyjr@protonmail.com",
+    description="A simple and power tool to mange ssh server remotely with GPG.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antony-jr/shell-control",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+    ],
+    python_requires='>=3.6',
+)
